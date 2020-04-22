8a3ca2f471cbb6641f339bad9b622a2c5fca8743
marlon <marlon_10993x@hotmail.com>
2020-04-22 18:29:41 -0500

    definiendo nuestro template para el admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,6 +5,7 @@ Class Admin extends CI_Controller {
     public function __construct() {
         parent::__construct();
         
+        $this->load->library("parser");
         $this->load->helper("url");
         $this->load->helper("form");
         $this->load->helper("Post_helper");
@@ -15,12 +16,17 @@ Class Admin extends CI_Controller {
     }
     
     public function post_list(){
-        $this->load->view("admin/post/list");
+        $view["body"] = $this->load->view("admin/post/list", NULL, TRUE);
+        $view["title"] = "Posts";
+        $this->parser->parse("admin/template/body", $view);
     }
     
     public function post_save(){
         $data["data_posted"] = posted();
-        $this->load->view("admin/post/save", $data);
+        $view["body"] = $this->load->view("admin/post/save", $data, TRUE);
+        $view["title"] = "Crear Post";
+        
+        $this->parser->parse("admin/template/body", $view);
     }
 
 }
